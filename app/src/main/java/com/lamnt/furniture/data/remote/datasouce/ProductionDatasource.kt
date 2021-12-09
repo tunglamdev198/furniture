f171b1aa4eb29e4495b90fe48593f75467c95af3
quanpersie2001 <quanpersie001@gmail.com>
2021-12-10 01:34:10 +0700

    Change fake data

--- app/src/main/java/com/lamnt/furniture/data/remote/datasouce/ProductionDatasource.kt
+++ app/src/main/java/com/lamnt/furniture/data/remote/datasouce/ProductionDatasource.kt
@@ -4,10 +4,10 @@ import com.lamnt.furniture.model.dto.Production
 
 class ProductionDatasource {
     fun getHomeProductions() = arrayListOf(
-        Production(1, "Chair 1", "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.3f, 200000f),
-        Production(2, "Chair 2", "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 3.5f, 200000f),
-        Production(3, "Chair 3", "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.8f, 200000f),
-        Production(4, "Chair 4", "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 2.5f, 200000f),
-        Production(5, "Chair 5", "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.0f, 200000f),
+        Production(1, "Chair 1", "", 20f, "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.3f, 1, 1, 100, 1, "This is Chair 1", 1639074605093, 1639074605093),
+        Production(2, "Chair 2", "", 25f, "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.5f, 1, 1, 120, 1, "This is Chair 2", 1639074605093, 1639074605093),
+        Production(3, "Chair 3", "", 30f, "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.8f, 1, 1, 95, 1, "This is Chair 3", 1639074605093, 1639074605093),
+        Production(4, "Chair 4", "", 21f, "https://cdn.shopify.com/s/files/1/2660/5106/products/nezid69eg3xecfcezu46_1400x.jpg?v=1569295596", 4.0f, 1, 1, 80, 1, "This is Chair 4", 1639074605093, 1639074605093),
+
     )
 }
