40df3c9ca6724e7027b16fe216d6564e93d84865
quanpersie2001 <quanpersie001@gmail.com>
2021-12-10 01:38:52 +0700

    Update CartAdapter

--- app/src/main/java/com/lamnt/furniture/ui/main/cart/CartAdapter.kt
+++ app/src/main/java/com/lamnt/furniture/ui/main/cart/CartAdapter.kt
@@ -3,13 +3,13 @@ package com.lamnt.furniture.ui.main.cart
 import com.lamnt.furniture.ui.base.BaseAdapter
 import com.lamnt.furniture.R
 import com.lamnt.furniture.databinding.ItemCartBinding
-import com.lamnt.furniture.model.Product
+import com.lamnt.furniture.model.dto.Production
 
-class CartAdapter : BaseAdapter<Product, ItemCartBinding>(){
+class CartAdapter : BaseAdapter<Production, ItemCartBinding>(){
     override val itemLayout: Int
         get() = R.layout.item_cart
 
-    override fun bind(binding: ItemCartBinding, data: Product, position: Int) {
-
+    override fun bind(binding: ItemCartBinding, data: Production, position: Int) {
+        binding.production = data
     }
 }
