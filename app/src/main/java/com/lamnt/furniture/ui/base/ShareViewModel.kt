af52bdd8893806d1794ff1a0a650f97f13abd619
quanpersie2001 <quanpersie001@gmail.com>
2021-12-11 03:50:14 +0700

    Use ShareViewModel

--- app/src/main/java/com/lamnt/furniture/ui/base/ShareViewModel.kt
+++ app/src/main/java/com/lamnt/furniture/ui/base/ShareViewModel.kt
@@ -1,6 +1,9 @@
 package com.lamnt.furniture.ui.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.lamnt.furniture.model.dto.Production
 
 class ShareViewModel : ViewModel() {
+    var production =  MutableLiveData<Production>()
 }
