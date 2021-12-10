19c89c8d0812b2ebc501159c3a36cf28894c9815
quanpersie2001 <quanpersie001@gmail.com>
2021-12-11 03:44:28 +0700

    update detail screen

--- app/src/main/java/com/lamnt/furniture/ui/main/detail/DetailFragment.kt
+++ app/src/main/java/com/lamnt/furniture/ui/main/detail/DetailFragment.kt
@@ -1,8 +1,12 @@
 package com.lamnt.furniture.ui.main.detail
 
 import android.os.Bundle
+import com.lamnt.furniture.MainActivity
 import com.lamnt.furniture.R
 import com.lamnt.furniture.databinding.FragmentDetailBinding
+import com.lamnt.furniture.extensions.changeTitle
+import com.lamnt.furniture.extensions.showBottomBar
+import com.lamnt.furniture.model.dto.Production
 import com.lamnt.furniture.ui.base.BaseFragmentMVVM
 import com.lamnt.furniture.ui.main.home.HomeProductionAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,10 +22,20 @@ class DetailFragment: BaseFragmentMVVM<FragmentDetailBinding, DetailViewModel>()
     override fun getViewModelClazz(): Class<DetailViewModel> = DetailViewModel::class.java
 
     override fun onViewReady(savedInstance: Bundle?) {
-
+        binding.production = shareViewModel.production.value
     }
 
     override fun initSubscriber() {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        with((requireActivity() as MainActivity)) {
+            showBottomBar(false)
+            changeTitle(shareViewModel.production.value?.name.toString())
+        }
+        
     }
 }
