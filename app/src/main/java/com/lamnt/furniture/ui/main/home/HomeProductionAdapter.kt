973941a83abc7cc125fd8a6287eecc5300906b43
quanpersie2001 <quanpersie001@gmail.com>
2021-12-10 15:56:24 +0700

    onItemClick in HomeProductionAdapter

--- app/src/main/java/com/lamnt/furniture/ui/main/home/HomeProductionAdapter.kt
+++ app/src/main/java/com/lamnt/furniture/ui/main/home/HomeProductionAdapter.kt
@@ -2,8 +2,10 @@ package com.lamnt.furniture.ui.main.home
 
 import com.lamnt.furniture.R
 import com.lamnt.furniture.databinding.ItemHomeProductionBinding
+import com.lamnt.furniture.extensions.click
 import com.lamnt.furniture.model.dto.Production
 import com.lamnt.furniture.ui.base.BaseAdapter
+import com.lamnt.furniture.ui.base.OnItemClickListener
 
 class HomeProductionAdapter : BaseAdapter<Production, ItemHomeProductionBinding>() {
     override val itemLayout: Int
@@ -11,5 +13,8 @@ class HomeProductionAdapter : BaseAdapter<Production, ItemHomeProductionBinding>
 
     override fun bind(binding: ItemHomeProductionBinding, data: Production, position: Int) {
         binding.production = data
+        binding.root.click { onItemClickListener?.onItemClick(binding.root, data, position) }
     }
+
+
 }
